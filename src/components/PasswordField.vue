5679684208fcc0b5d3f9813ea5864a5fffc8da53
bufgix <ooruc471@yandex.com>
2020-04-21 00:41:06 +0300

    Copy button ~ II

--- src/components/PasswordField.vue
+++ src/components/PasswordField.vue
@@ -1,24 +1,47 @@
 <template>
   <div class="uk-flex uk-flex-between">
     <input class="uk-input uk-form-blank uk-form-width-small" :type="type" :value="value" />
-    <button class="uk-icon-button" :uk-icon="lockBtnIcon" @click="showPassword()"></button>
-    <button class="uk-icon-button" uk-icon="copy"></button>
+    <button
+      v-if="isPassword"
+      class="uk-icon-button"
+      :uk-icon="lockBtnIcon"
+      @click="showPassword()"
+    ></button>
+    <button class="uk-icon-button" :uk-icon="copyBtnIcon" @click="copyPassword()"></button>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    value: String
+    value: String,
+    isPassword: {
+      type: Boolean,
+      default: true
+    }
   },
   data: () => ({
     type: "password",
-    lockBtnIcon: "lock"
+    lockBtnIcon: "lock",
+    copyBtnIcon: "copy"
   }),
   methods: {
     showPassword: function() {
       this.type = this.type === "password" ? "text" : "password";
       this.lockBtnIcon = this.type === "password" ? "lock" : "unlock";
+    },
+    copyPassword: function() {
+      this.$copyText(this.value)
+        .then(() => {
+          this.copyBtnIcon = "check";
+          setTimeout(() => {
+            this.copyBtnIcon = "copy";
+          }, 5000);
+        })
+        .catch(err => {
+          this.copyBtnIcon = "close";
+          console.log(err);
+        });
     }
   }
 };
