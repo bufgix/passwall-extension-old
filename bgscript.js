a290157ffc72726444383496ab76c6ec9788e1bd
bufgix <ooruc471@yandex.com>
2020-04-22 04:04:50 +0300

    testing bgscript

---
+++ bgscript.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/* function logURL(requestDetails) {
+    if (requestDetails.method === "POST"){
+        console.log("Loading: " + JSON.stringify(requestDetails));
+    }
+ 
+}
+
+browser.webRequest.onBeforeRequest.addListener(logURL, {
+  urls: ["<all_urls>"],
+});
+ */
