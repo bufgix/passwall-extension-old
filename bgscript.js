dcd19d3f42ea83a64dda830f139cde21e6fc8e48
bufgix <ooruc471@yandex.com>
2020-04-23 05:58:44 +0300

    content script config

--- bgscript.js
+++ bgscript.js
@@ -1,12 +1,1 @@
-/* eslint-disable no-undef */
-/* function logURL(requestDetails) {
-    if (requestDetails.method === "POST"){
-        console.log("Loading: " + JSON.stringify(requestDetails));
-    }
- 
-}
-
-browser.webRequest.onBeforeRequest.addListener(logURL, {
-  urls: ["<all_urls>"],
-});
- */
+console.log("Passwall extension background script is running");
